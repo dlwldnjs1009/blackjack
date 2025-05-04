effe736d46a7de7ff2afad7ae3cfe50133f17945
LeeJiWon <dlwldnjs1009@gmail.com>
2025-05-04 18:38:05 +0900

    chore: 주석 수정

--- src/Deck.java
+++ src/Deck.java
@@ -3,9 +3,7 @@ import java.util.Stack;
 
 public class Deck {
 	/**
-	 * todo: 가독성 좋게 문장 가다듬기
-	 * 보통 카드 더미를 생각하면 카드를 섞고 앞면(카드번호, 무늬)이 보이는 면이 아래에 깔리게 뒤집고
-	 * 맨 밑에 깔려 있는 카드부터 뽑히게 된다. Stack 형태라 생각했다.
+	 * 카드 더미는 Stack 형태로 구현되며, 카드를 섞은 후 맨 위에서부터 뽑습니다.
 	 */
 	private final Stack<Card> cards = new Stack<>();
 
