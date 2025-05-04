f6b28df5ba9330c31289185299e321007de863e2
LeeJiWon <dlwldnjs1009@gmail.com>
2025-05-04 16:22:35 +0900

    feat: Hand 클래스 점수 계산, 블랙잭/버스트 판단 기능 구현

---
+++ src/Hand.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hand {
+	private final List<Card> cards = new ArrayList<>();
+
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+
+	public int getScore() {
+		int score = 0;
+		int aceCount = 0;
+
+		for (Card card : cards) {
+			score += card.getNumber().getMinValue(); // todo: . 줄이는 방법 생각
+			if (card.getNumber().isAce()) {
+				aceCount++;
+			}
+		}
+		// Ace 하나를 11로 계산했을 때 21 이하라면 11로 처리
+		if (aceCount > 0 && score + 10 <= 21) {
+			return score + 10;
+		}
+		return score; // Ace가 없거나 Ace를 1로 처리했을 때
+	}
+
+	public boolean isBurst() {
+		return getScore() > 21;
+	}
+
+	public boolean isBlackjack() {
+		return cards.size() == 2 && getScore() == 21;
+	}
+
+	public List<Card> getCards() {
+		return Collections.unmodifiableList(cards);
+	}
+
+}
